008869560ecc2d640787ed767214c0c6e7810342
Mitchell Scholer <mitchell@financr.com.au>
2020-08-26 15:30:44 +1000

    v3

--- project/js/main.js
+++ project/js/main.js
@@ -3,12 +3,18 @@ $(document).ready(function(){
 })
 
 function getWeather(searchQuery) {
-  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=metric&appid=a0877609d325ff8690cdcd7695c0879d"
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=metric&appid="+apiKey
+
+  $(".city").text("");
+  $(".temp").text("");
+  $(".error-message").text("");
 
   $.ajax(url,{success: function(data){
     $(".city").text(data.city);
     $(".temp").text(data.main.temp);
-  }})
+  }, error: function(error){
+    $(".error-message").text("An error occured");
+}})
 }
 
 function searchWeather(){
